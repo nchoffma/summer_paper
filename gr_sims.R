b43d0c776fc875f3ddefdaf57ad825314dea8dbb
Nick Hoffman <Nick@Nicks-MacBook-Pro.local>
2020-03-25 10:10:05 -0400

    finished simulations and charts (for now)

--- gr_sims.R
+++ gr_sims.R
@@ -3,7 +3,14 @@
 
 # Replicates model from Genicot and Ray (2017)
 
+library(tidyverse)
 library(purrr)
+library(ggplot2)
+library(ggthemes)
+theme_set(theme_tufte() + theme(
+  axis.line = element_line(color = "black")
+))
+fig_path <- "figures/"
 
 # Setup and Params --------------------------------------------------------
 
@@ -17,6 +24,7 @@ big_a <- 4
 rho <- 1.6
 bet <- 0.55
 theta <- 1.0
+cegm <- T # use CEGM?
 
 # Functions ---------------------------------------------------------------
 
@@ -26,8 +34,13 @@ w0 <- function(z) {del * z ^ (1 - sig)}
 w1 <- function(e) {del * pi * e ^ (1 - sig)}
 
 # Production functions
-prod <- function(k) theta * (big_a / bet) * (k ^ bet)
-kfunc <- function(z) (bet * z / (theta * big_a)) ^ (1 / bet) # inverse of prod()
+if (cegm) {
+  prod <- function(k) {rho * k}
+  kfunc <- function(z) { z / rho}
+} else {
+  prod <- function(k) theta * (big_a / bet) * (k ^ bet)
+  kfunc <- function(z) (bet * z / (theta * big_a)) ^ (1 / bet) # inverse of prod()
+}
 
 # Total Payoff
 total_payoff <- function(y, z, a) {
@@ -38,7 +51,12 @@ total_payoff <- function(y, z, a) {
 util_p <- function(c) {(1 - sig) * c ^ -sig}
 w0_p <- function(z) {del * (1 - sig) * z ^ -sig}
 w1_p <- function(e) {del * pi * (1 - sig) * e ^ -sig}
-f_p <- function(k) {theta * big_a * k ^ (bet - 1)}
+
+if (cegm) {
+  f_p <- function(k) {rho}
+} else {
+  f_p <- function(k) {theta * big_a * k ^ (bet - 1)}
+}
 
 # Optimal z
 zopt0 <- function(z, y) { # z < a
@@ -112,10 +130,107 @@ find_z <- function(y, a) {
   }
 }
 
+# Payoff and Cost Plots ---------------------------------------------------
+
+# Figure 1: Aspirations and Payoffs
+nzpts <- 500
+a <- 20
+y <- 25
+fig_data <- tibble(
+  z = seq(0, 50, length.out = nzpts),
+  payoff = w0(z) + w1(pmax(c(z - a), 0)),
+  cost = util(y) - util(y - kfunc(z))
+)
+
+ggplot(fig_data, aes(z, payoff)) + 
+  geom_line() + 
+  geom_vline(xintercept = a, linetype = "dashed") + 
+  coord_cartesian(xlim = c(0, 40)) + 
+  labs(
+    title = "Figure 1: Payoffs with Aspirations",
+    y = "Payoff", 
+    caption = paste0("y = ", y, "; a = ", a)
+  ) + 
+  annotate(
+    "text", x = 10, y = 1.5, 
+    label = expression(w[0](z))
+  ) + 
+  annotate(
+    "text", x = 30, y = 2.5, 
+    label = expression(w[0](z) + w[1](z - a))
+  )
+
+ggsave(paste0(fig_path, "fig1.jpg"))
+
+ggplot(fig_data, aes(z, payoff)) + 
+  geom_line() + 
+  geom_vline(xintercept = a, linetype = "dashed") + 
+  geom_line(aes(y = cost)) + 
+  coord_cartesian(xlim = c(0, 40)) + 
+  labs(
+    title = "Figure 2: Payoffs and Costs",
+    subtitle = "Problem is Concave on either side of a",
+    y = element_blank(), 
+    caption = paste0("y = ", y, "; a = ", a)
+  ) + 
+  annotate(
+    "text", x = 10, y = 1.5, 
+    label = expression(w[0](z))
+  ) + 
+  annotate(
+    "text", x = 30, y = 2.5, 
+    label = expression(w[0](z) + w[1](z - a))
+  ) + 
+  annotate(
+    "text", x = 30, y = 0.55, 
+    label = expression(u(y) - u(y - k(z)))
+  )
+
+ggsave(paste0(fig_path, "fig2.jpg"))
+
+
+# Threshold y* ------------------------------------------------------------
+
+y_init <- seq(5.0, 100.0, length.out = 100) # uniform dist
+a_y <- 0.5 * (y_init + mean(y_init))
+opt_z <- map2_dbl(y_init, a_y, find_z)
+opt_g <- opt_z / y_init
+
+fig4_data <- tibble(
+  y = y_init,
+  asp = a_y,
+  opt_z = opt_z,
+  opt_g = opt_g
+)
+
+fig4_data <- fig4_data %>% 
+  mutate(
+    g_f = if_else(opt_z < a, opt_g, NaN), # frustrated
+    g_s = if_else(opt_z >= a, opt_g, NaN)
+  )
+
+ggplot(fig4_data, aes(x = y)) + 
+  geom_line(aes(y = g_f)) + 
+  geom_line(aes(y = g_s)) + 
+  geom_vline(xintercept = 24.5, linetype = "dashed") + 
+  labs(
+    title = "Figure 4: Growth and Income threshold y*",
+    y = "g"
+  ) + 
+  annotate(
+    "text", x = 15, y = 0.78, 
+    label = expression(underline(g))
+  ) + 
+  annotate(
+    "text", x = 60, y = 1.4, 
+    label = "g(r)"
+  )
+ggsave(paste0(fig_path, "fig4.jpg"))
+
 # Example 1 ---------------------------------------------------------------
 
 # Deterministic production
-y_init <- seq(10.0, 130.0, length.out = 100) # uniform dist
+y_init <- seq(10.0, 100.0, length.out = 100) # uniform dist
 yvals <- y_init
 
 # Find steady state
@@ -139,11 +254,63 @@ plot(y_init, yvals)
 hist(yvals, breaks = length(y_init))
 p_new <- mean(round(yvals, 2) == min(round(yvals, 2)))
 
-# whether incomes center around one or two values depends on the spread 
-# of the initial distribution
-# as does p_new
+# Figure 5: Varying p -----------------------------------------------------
 
-# Example 2: Introduce Heterogeneity --------------------------------------
+top_vals <- seq(100, 120, by = 1)
+
+find_ss <- function(top) {
+  y_init = seq(10, top, length.out = 100) # uniform dist
+  yvals = y_init
+  
+  # Find steady state
+  dif = 10.0
+  tol = 1e-3
+  its = 0
+  maxit = 100
+  
+  while (dif > tol & its < maxit) {
+    avg_inc = mean(yvals)
+    a_y = 0.5 * (yvals + avg_inc)
+    
+    yvals_next = map2_dbl(yvals, a_y, find_z)
+    
+    dif = max(abs(yvals - yvals_next))
+    its = its + 1
+    yvals = yvals_next
+  }
+  
+  p_new = mean(round(yvals, 2) == min(round(yvals, 2)))
+  return(c(
+    y_h = max(yvals),
+    y_l = min(yvals),
+    p = p_new,
+    its = its
+  ))
+}
+
+fig5_data <- map(top_vals, find_ss)
+fig5_data <- bind_rows(lapply(fig5_data, as.data.frame.list))
+
+fig5_data <- fig5_data %>% 
+  mutate(
+    mean_inc = p * y_l + (1 - p) * y_h,
+    asp_l = 0.5 * (y_l + mean_inc),
+    asp_h = 0.5 * (y_h + mean_inc)
+  )
+
+ggplot(fig5_data, aes(x = p)) + 
+  geom_line(aes(y = y_l, color = "y_l")) + 
+  geom_line(aes(y = asp_l, color = "a_l"), linetype = "dashed") +  
+  geom_line(aes(y = y_h, color = "y_h")) + 
+  geom_line(aes(y = asp_h, color = "a_h"), linetype = "dashed") + 
+  labs(
+    title = "Figure 5: Bimodal Distributions",
+    x = "p", y = "Income", color = element_blank()
+  )
+
+ggsave(paste0(fig_path, "fig5.jpg"))
+
+# Example 2: Heterogeneity (TODO) -----------------------------------------
 
 # Stochastic production
 y_init <- seq(10.0, 1000.0, length.out = 100) # uniform dist
@@ -163,9 +330,9 @@ while (dif > tol & its < maxit) {
   
   yvals_next <- map2_dbl(yvals, a_y, find_z)
   
-  
-  print(dif)
   its <- its + 1
   yvals <- yvals_next
-  p_old <- p_next
 }
+
+# Q: How are they getting the distribution
+
